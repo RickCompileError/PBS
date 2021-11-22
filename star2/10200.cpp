da4b95448b2891965f084782df32f8299fee76de
CompileError <gngn15401@gmail.com>
2021-11-22 22:25:09 +0800

    add build prime table function

--- star2/10200.cpp
+++ star2/10200.cpp
@@ -3,9 +3,20 @@
 using namespace std;
 
 bool func[10001]{false};
+vector<int> prime;
+
+void build_prime(){
+    vector<bool> p(10001,true);
+    p[0] = p[1] = false;
+    for (int i=2;i*i<p.size();i++){
+        if (p[i]) for (int j=i*i;j<p.size();j+=i) p[j] = false;
+    }
+    for (int i=2;i<p.size();i++) if (p[i]) prime.push_back(i);
+
+}
 
 bool isPrime(int n){
-    for (int i=2;i*i<=n;i++) if (n%i==0) return false;
+    for (int i=0;i<prime.size() && prime[i]*prime[i]<=n;i++) if (n%prime[i]==0) return false;
     return true;
 }
 
@@ -17,6 +28,7 @@ void build(){
 }
 
 int main(){
+    build_prime();
     build();
     int a,b;
     while (cin >>a >>b){
