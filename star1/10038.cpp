7a9355a87cb73f7d2d06d13757028eb2da6b3072
CompileError <gngn15401@gmail.com>
2021-11-22 21:32:29 +0800

    more simplify

--- star1/10038.cpp
+++ star1/10038.cpp
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h>
 
+#define ll long long
+
 using namespace std;
 
 int main(){
@@ -9,14 +11,14 @@ int main(){
             cout <<"Not jolly\n";
             continue;
         }
-        int seq[n];
+        ll seq[n];
         bool exist[n]{0};
         bool legal = true;
         for (int i=0;i<n;i++){
             cin >>seq[i];
             if (i==0) continue;
-            int dif = abs(seq[i]-seq[i-1]);
-            if (n<=0 || dif>n-1 || dif==0 || exist[dif]) legal = false;
+            ll dif = abs(seq[i]-seq[i-1]);
+            if (dif>n-1 || dif==0 || exist[dif]) legal = false;
             else exist[dif] = true;
         }
         if (legal) cout <<"Jolly\n";
