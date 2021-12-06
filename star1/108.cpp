b547681455d616e4a46f97e49d884e91f8f2a7a4
CompileError <gngn15401@gmail.com>
2021-12-06 17:26:51 +0800

    another solution

--- star1/108.cpp
+++ star1/108.cpp
@@ -5,13 +5,14 @@ using namespace std;
 const int mxN = 1e2;
 int arr[mxN+1][mxN+1]{{}};
 
-int main(){
+void solution1(){
     int n; cin >>n;
-    for (int i=1;i<=n;i++)
+    for (int i=1;i<=n;i++){
         for (int j=1;j<=n;j++){
             cin >>arr[i][j];
             arr[i][j] += arr[i][j-1];
         }
+    }
     for (int i=1;i<=n;i++)
         for (int j=1;j<=n;j++)
             arr[i][j] += arr[i-1][j];
@@ -22,4 +23,30 @@ int main(){
                 for (int y=0;y<=j-1;y++)
                     ans = max(ans,arr[i][j]-arr[x][j]-arr[i][y]+arr[x][y]);
     cout <<ans <<endl;
+}
+
+void solution2(){
+    int n; cin >>n;
+    for (int i=1;i<=n;i++){
+        for (int j=1;j<=n;j++){
+            cin >>arr[i][j];
+            arr[i][j] += arr[i-1][j];
+        }
+    }
+    int ans = INT_MIN;
+    for (int i=1;i<=n;i++){
+        for (int j=i;j<=n;j++){
+            int cur = 0;
+            for (int k=1;k<=n;k++){
+                cur += arr[j][k] - arr[i-1][k];
+                ans = max(ans,cur);
+                if (cur<0) cur = 0;
+            }
+        }
+    }
+    cout <<ans <<endl;
+}
+
+int main(){
+    solution2();
 }
