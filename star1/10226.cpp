b8acd721792beee124edac8c55811fada3dcc2f2
CompileError <gngn15401@gmail.com>
2021-12-14 20:56:44 +0800

    more precise code

--- star1/10226.cpp
+++ star1/10226.cpp
@@ -2,10 +2,6 @@
 
 using namespace std;
 
-bool cmp(pair<string,int> a, pair<string,int> b){
-    return a.first<b.first;
-}
-
 int main(){
     int n; cin >>n; getchar(), getchar();
     string tree;
@@ -15,9 +11,7 @@ int main(){
         double count = 0;
         map<string,int> m;
         while (getline(cin,tree,'\n') && tree!="") m[tree]++, count++;
-        vector<pair<string,int>> v(m.begin(),m.end());
-        sort(v.begin(),v.end());
-        for (pair<string,int> i: v) cout <<i.first <<" " <<setprecision(4) <<fixed <<i.second/count*100 <<endl;
+        for (auto& i: m) cout <<i.first <<" " <<setprecision(4) <<fixed <<i.second/count*100 <<endl;
         not_first = true;
     }
 }
