8f34eecea0314079a0742835ea8d1de5ad3ee4d2
CompileError <gngn15401@gmail.com>
2021-12-14 10:14:05 +0800

    Done

---
+++ star2/900.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+
+using namespace std;
+
+int main(){
+    int n;
+    ll ans[51]{0,1,2};
+    while (cin >>n && n!=0){
+        for (int i=3;i<51;i++) ans[i] = ans[i-1] + ans[i-2];
+        cout <<ans[n] <<endl;
+    }
+}
