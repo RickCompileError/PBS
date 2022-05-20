ab083cf498ce12fbb290ab16deb6e2c78d56b059
RickCompileError <gngn15401@gmail.com>
2022-05-21 03:57:28 +0800

    add comment and more precisely code

--- UVA/star4/12911.cpp
+++ UVA/star4/12911.cpp
@@ -7,32 +7,29 @@ using namespace std;
 const int mxN = 40;
 
 int main(){
-    int N, lsize, rsize;
-    ll T, s[mxN+1];
+    int N;
+    ll T;
     while (cin >>N >>T){
-        memset(s,0,sizeof(s));
-        if ((lsize=N/2)==0) lsize = 1;
-        rsize = N - lsize;
+        // split into two subsets because 2^40 will timeout
+        int lsize, rsize;
+        if ((lsize=N/2)==0) lsize = 1, rsize = N - lsize;
+        ll s[mxN+1];
         for (int i=1;i<=N;i++) cin >>s[i];
         map<ll,int> lcount, rcount;
+        // use bit to represent the choosing index in left subset
         for (int i=1;i<(1<<lsize);i++){
             ll sum = 0;
-            for (int j=1;j<=lsize;j++){
-                if ((i&(1<<j-1)) != 0) sum += s[j];
-            }
+            for (int j=0;j<lsize;j++) if ((i&1<<j) != 0) sum += s[j];
             lcount[sum]++;
         }
+        // use bit to represent the choosing index in right subset
         for (int i=1;i<(1<<rsize);i++){
             ll sum = 0;
-            for (int j=1;j<=rsize;j++){
-                if ((i&(1<<j-1)) != 0) sum += s[lsize+j];
-            }
+            for (int j=0;j<rsize;j++) if ((i&1<<j) != 0) sum += s[lsize+j];
             rcount[sum]++;
         }
         ll ans = 0;
-        for (map<ll,int>::iterator i=lcount.begin();i!=lcount.end();i++){
-            ans += i->second*rcount[T-i->first];
-        }
+        for (pair<ll,int> i: lcount) ans += i.second * rcount[T-i.first];
         cout <<ans+lcount[T]+rcount[T] <<endl;
     }
 }
