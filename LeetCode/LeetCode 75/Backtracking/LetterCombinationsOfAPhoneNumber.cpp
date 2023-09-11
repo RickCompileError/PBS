b6e30feaa98bc426c8b2085a5fe57d43873c7a65
RickCompileError <gngn15401@gmail.com>
2023-09-11 22:57:50 +0800

    Done

---
+++ LeetCode/LeetCode 75/Backtracking/LetterCombinationsOfAPhoneNumber.cpp
@@ -0,0 +1,23 @@
+// Time: O(4^n), Space: O(n)
+class Solution {
+private:
+    string alpha[8] = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+    void backtracking(vector<string>& ans, string digits, int pos = 0, string now = "") {
+        if (pos >= digits.size()) {
+            ans.push_back(now);
+            return;
+        }
+        for (char i: alpha[digits[pos] - '2']) {
+            backtracking(ans, digits, pos + 1, now + i);
+        }
+    }
+
+public:
+    vector<string> letterCombinations(string digits) {
+        if (digits.size() == 0) return vector<string>();
+        vector<string> ans;
+        backtracking(ans, digits);
+        return ans;
+    }
+};
