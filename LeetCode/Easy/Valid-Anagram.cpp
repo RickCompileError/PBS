c2316486635876fc69daa4b652b6402e3350822c
RickCompileError <gngn15401@gmail.com>
2024-11-20 22:49:10 +0800

    No. 242 Opt

--- LeetCode/Easy/Valid-Anagram.cpp
+++ LeetCode/Easy/Valid-Anagram.cpp
@@ -1,3 +1,4 @@
+// O(nlogn)
 class Solution {
 public:
     bool isAnagram(string s, string t) {
@@ -5,5 +6,20 @@ public:
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
         return s == t;
+    }
+};
+
+// O(n)
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char, int> u;
+        for (auto c: s) u[c]++;
+
+        for (auto c: t) u[c]--;
+
+        for (auto c: u) if (c.second) return false;
+
+        return true;
     }
 };
