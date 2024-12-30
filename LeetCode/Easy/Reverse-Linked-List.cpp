611bc0b96e7117f757450b97265129e7f0d70185
RickCompileError <gngn15401@gmail.com>
2024-12-30 22:39:17 +0800

    No. 206

---
+++ LeetCode/Easy/Reverse-Linked-List.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* curr = nullptr;
+        ListNode* prev = nullptr;
+        while (head) {
+            curr = head;
+            head = head -> next;
+            curr -> next = prev;
+            prev = curr;
+        }
+
+        return curr;
+    }
+};
