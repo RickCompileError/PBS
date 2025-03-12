980cf69eccc99e1eb8c2fcfd5a19f6feb85fafb9
RickCompileError <gngn15401@gmail.com>
2025-03-12 19:05:30 +0800

    No. 746

---
+++ LeetCode/Easy/Min-Cost-Climbing-Stairs.cpp
@@ -0,0 +1,11 @@
+// Time: O(n), Space: O(1)
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int sz = cost.size();
+        for (int i = 2; i < sz; i++) {
+            cost[i] = cost[i] + min(cost[i-1], cost[i-2]);
+        }
+        return min(cost[sz-1], cost[sz-2]);
+    }
+};
