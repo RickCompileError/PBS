9cc971229ccafb46c370cc45f80d9d5d2d64c657
RickCompileError <gngn15401@gmail.com>
2025-06-01 17:44:13 +0800

    No. 2929

---
+++ LeetCode/Medium/Distribute-Candies-Among-Children-II.cpp
@@ -0,0 +1,13 @@
+// Time: O(1), Space: O(1)
+class Solution {
+public:
+    long long distributeCandies(int n, int limit) {
+        long long res = 0;
+        for (int i = 0; i <= min(limit, n); i++) {
+            if (n - i <= 2 * limit) {
+                res += min(n - i, limit) - max(0, n - i - limit) + 1;
+            }
+        }
+        return res;
+    }
+};
