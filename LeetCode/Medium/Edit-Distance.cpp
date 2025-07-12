eea5705baf0b9dc57e9376bc295caa05e02d17f8
RickExpertBook <gngn15401@gmail.com>
2025-07-12 22:39:30 +0800

    Edit No. 72 again

--- LeetCode/Medium/Edit-Distance.cpp
+++ LeetCode/Medium/Edit-Distance.cpp
@@ -56,5 +56,31 @@ public:
         return dp[0][0];
     }
 };
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int sz1 = word1.size(), sz2 = word2.size();
+        vector<vector<int>> dp(sz1 + 1, vector<int>(sz2 + 1, 0));
+
+        for (int i = 1; i <= sz1; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 1; j <= sz2; j++) {
+            dp[0][j] = j;
+        }
+        for (int i = 1; i <= sz1; i++) {
+            for (int j = 1; j <= sz2; j++) {
+                if (word1[i - 1] == word2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                    continue;
+                }
+                dp[i][j] = min(dp[i - 1][j - 1], min(dp[i - 1][j], dp[i][j - 1]));
+                dp[i][j]++;
+            }
+        }
+
+        return dp[sz1][sz2];
+    }
+};
 
 // It has a optimal version, Time: O(m*n), Space: O(min(m,n))
