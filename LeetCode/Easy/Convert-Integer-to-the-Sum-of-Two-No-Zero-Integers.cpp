c420437b463d6a21d17aaac9b8ae71f957255395
RickCompileError <gngn15401@gmail.com>
2025-09-13 15:40:52 +0800

    No. 1317

---
+++ LeetCode/Easy/Convert-Integer-to-the-Sum-of-Two-No-Zero-Integers.cpp
@@ -0,0 +1,22 @@
+class Solution {
+private:
+    bool noZero(int n) {
+        while (n > 0) {
+            if (n % 10 == 0) {
+                return false;
+            }
+            n /= 10;
+        }
+        return true;
+    }
+
+public:
+    vector<int> getNoZeroIntegers(int n) {
+        for (int i = 1; i < n; i++) {
+            if (noZero(i) && noZero(n - i)) {
+                return {i, n - i};
+            }
+        }
+        return {};
+    }
+};
